99c6d2a2f97e0ea8f1daec47f1c68cb774dc0055
dex11 <nick.jebirashvili@gmail.com>
2020-11-05 00:18:26 +0400

    validation inplementation v1

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const signInBtns = $("button[id^=signIn]");
-// signInBtns.onClick = signIn;
+const forms = $(".landing");
+if (forms.length > 0) {
+    forms[0].addEventListener("submit", signIn);
+}
 
 function register() {
+    console.log("Wtf");
     window.location = "registration.html";
+}
+
+function signIn(e) {
+    const inputs = $(".input_txt");
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.length == 0) e.preventDefault();
+    }
 }
