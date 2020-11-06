b84ffa1b80875869ae7aa46c9741ef8ed6d69746
dex11 <nick.jebirashvili@gmail.com>
2020-11-06 12:55:57 +0400

    working on a filter bar

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const forms = $(".landing");
+const dropDowns = $(".dropdown_option");
+
 if (forms.length > 0) {
     forms[0].addEventListener("submit", signIn);
+}
+
+for (let i = 0; i < dropDowns.length; i++) {
+    dropDowns[i].addEventListener("click", dropDownSelect);
 }
 
 function register() {
@@ -13,4 +19,11 @@ function signIn(e) {
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].value.length == 0) e.preventDefault();
     }
+}
+
+function dropDownSelect(e) {
+    let b = $(this).closest("button");
+    b.innerText = "wdwdw";
+    console.log(b.innerHTML);
+    // e.target.closest("#dropbtn").innerHTML = e.target.innerHTML;
 }
